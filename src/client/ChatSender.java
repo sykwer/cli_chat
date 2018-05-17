df7bbd23f2da604ba8ee207d68e45b35dfecfbf6
Marcygit <sano.utokyo@gmail.com>
2018-05-17 15:06:25 +0900

    Add skeleton

--- src/client/ChatSender.java
+++ src/client/ChatSender.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.net.Socket;
 
+class ChatSender {
+    private static ChatSender singleton = new ChatSender();
 
-public class ChatSender {
+    private ChatSender() {
+    }
 
+    static ChatSender getSender() {
+        return singleton;
+    }
+
+    // TODO: 2018/05/13 実装
+    void sendMessage(Socket socket, String message) throws IOException {
+    }
 }

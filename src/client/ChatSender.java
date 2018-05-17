8583a0946211a3ae1be75091a10b0e2fd99fcf24
Marcygit <sano.utokyo@gmail.com>
2018-05-17 16:16:08 +0900

    Last commit2

--- src/client/ChatSender.java
+++ src/client/ChatSender.java
@@ -3,21 +3,19 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 class ChatSender {
-    private static ChatSender singleton = new ChatSender();
+	private static ChatSender singleton = new ChatSender();
 
-    private ChatSender() {
-    }
+	private ChatSender() {
+	}
 
-    static ChatSender getSender() {
-        return singleton;
-    }
+	static ChatSender getSender() {
+		return singleton;
+	}
 
-    // TODO: 2018/05/13 実装
-    void sendMessage(Socket socket, String message) throws IOException {
-    		PrintWriter writer = new PrintWriter(socket.getOutputStream());
-    		System.out.println("送信メッセージ: " + message);
-    		writer.println(message);
-    		writer.close();
-    }
+	// TODO: 2018/05/13 実装
+	void sendMessage(Socket socket, String message) throws IOException {
+		PrintWriter writer = new PrintWriter(socket.getOutputStream());
+		writer.println(message);
+		writer.close();
+	}
 }
-

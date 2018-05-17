219fe3732824fb23d9af6fb14692014f0ca103ae
hiramekun <thescript1210@gmail.com>
2018-05-17 17:11:47 +0900

    Implment swich by commands

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -23,21 +23,42 @@ class ChatClient {
 
     private void waitForCommands() {
         Scanner scanner = new Scanner(System.in);
+
+        WHILE:
         while (true) {
             String[] args = scanner.nextLine().trim().split("\\s");
+            Command command = Command.valueOf(args[0]);
+            switch (command) {
+                case CHATLOGIN:
+                    if (socket != null && socket.isConnected()) {
 
-            // todo
-            try {
-                connectServer(args[2].split(":")[0], Integer.parseInt(args[2].split(":")[1]));
-            } catch (IOException e) {
-                e.printStackTrace();
+                    } else {
+                        // todo
+                        try {
+                            connectServer(args[2].split(":")[0], Integer.parseInt(args[2].split(":")[1]));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            sender.sendMessage(socket, args[4]);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+
+                case SEND:
+                    sender.sendMessage(socket, args[1]);
+                    break;
+                case LOGOUT:
+                    sender.sendMessage(socket, "logout");
+                    close();
+                    break;
+                case EXIT:
+                    break WHILE;
             }
 
-            try {
-                sender.sendMessage(socket, args[4]);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 

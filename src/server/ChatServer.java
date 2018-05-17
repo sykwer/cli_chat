5d75324756feaf8a60f20cc208bbe4c10facd180
vagrant <vagrant@localhost.localdomain>
2018-05-17 05:27:58 +0000

    Implementing singleton

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,6 +1,22 @@
 public class ChatServer {
+
+  private static ChatServer app;
+
+  public static ChatServer getApp() {
+    if (this.app == null) {
+      this.app = new ChatServer();
+      return this.app;
+    }
+    return this.app;
+  }
+
+  private void ChatServer();
+
+  public void start() {
+
+  }
+
   public static void main(String[] args) {
-    //ChatServer.getApp().start();
-    System.out.println("Hello");
+    ChatServer.getApp().start();
   }
 }

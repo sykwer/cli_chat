c55dba8e9adb77c9d6021170507210157ccfaae3
hiramekun <thescript1210@gmail.com>
2018-05-17 16:04:08 +0900

    Initial commit

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,5 +1,25 @@
 class ChatClient {
-  public static void main(String[] args) {
-	  System.out.println("Hello");
-  }
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient();
+        client.waitForCommands();
+    }
+    
+    private enum Command {
+        CHATLOGIN,
+        SEND,
+        LOGOUT,
+        EXIT
+    }
+
+    private ChatClient() {
+    }
+
+    private void waitForCommands() {
+    }
+
+    private void connectServer() {
+    }
+
+    private void close() {
+    }
 }

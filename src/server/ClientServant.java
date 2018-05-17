19b8ab69daf1bf070773f31bdb5cf223b67da4b4
vagrant <vagrant@localhost.localdomain>
2018-05-17 06:52:59 +0000

    implement the function to receive messages sent from client sockets

--- src/server/ClientServant.java
+++ src/server/ClientServant.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
 import java.net.Socket;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 class ClientServant extends Thread {
 
@@ -11,6 +15,22 @@ class ClientServant extends Thread {
   }
 
   public void start() {
+    try {
+      InputStream is = this.clientSocket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
+      while (!this.clientSocket.isClosed()) {
+        String[] msg = reader.readLine().split(" ", 2);
+        handleCommand(msg[0], msg[1]);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
+
+  private void handleCommand(String cmd, String value) {
+    System.out.println("cmd : "+cmd);
+    System.out.println("value : "+value);
+  }
+
 }

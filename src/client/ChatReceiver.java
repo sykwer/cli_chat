2a92c2de18306a6966299a1ae19789221ac6764d
杉浦竜 <>
2018-05-24 13:32:02 +0900

    Implement read one line

--- src/client/ChatReceiver.java
+++ src/client/ChatReceiver.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 class ChatReceiver {
@@ -12,6 +12,18 @@ class ChatReceiver {
     }
 
     // TODO: 2018/05/13 実装
-    void waiteForMessage(Socket socket) throws IOException {
+    void waiteForMessage(Socket socket, String message){
+        InputStream input = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(input);
+        BufferedReader  bufferedReader = new BufferedReader(inputStreamReader);
+        String line = bufferedReader.readLine();
+        System.out.println(line);
+        
+
+        /*
+        * やりたいこと
+        * 1. socketを用いて、入力を受けとる
+        * 2. 入力を受け取ったら、それを標準出力で表示する
+        * */
     }
 }

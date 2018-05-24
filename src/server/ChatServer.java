94ef3feeee63d66d12fa5e054143e26ee137382a
sykwer <sykwer@gmail.com>
2018-05-24 14:54:39 +0900

    Reformat

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,52 +1,50 @@
-import java.util.ArrayList;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import java.net.InetAddress;
+import java.net.*;
+import java.util.*;
+import java.io.*;
 
 public class ChatServer {
 
-  private static ChatServer app;
+    private static ChatServer app;
 
-  public static ChatServer getApp() {
-    if (app == null) {
-      app = new ChatServer();
-      return app;
+    public static ChatServer getApp() {
+        if (app == null) {
+            app = new ChatServer();
+            return app;
+        }
+        return app;
     }
-    return app;
-  }
 
 
-  private ServerSocket serverSocket;
-  public ArrayList<ClientServant> clientServants = new ArrayList<ClientServant>();
+    private ServerSocket serverSocket;
+    public ArrayList<ClientServant> clientServants = new ArrayList<ClientServant>();
 
-  private ChatServer() {};
-
-  public void start(int port) {
-    try {
-      this.serverSocket = new ServerSocket(port);
-
-      while (!this.serverSocket.isClosed()) {
-        Socket clientSocket = this.serverSocket.accept();
-
-        ClientServant cs = new ClientServant(clientSocket);
-        this.clientServants.add(cs);
-
-        cs.start();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    private ChatServer() {
     }
-  }
 
-  public static void main(String[] args) throws IOException {
-    int port = Integer.parseInt(args[0]);
-    System.out.println("My Address: " + InetAddress.getLocalHost().getHostAddress() + ":" + port);
-    ChatServer.getApp().start(port);
-  }
+    public void start(int port) {
+        try {
+            this.serverSocket = new ServerSocket(port);
 
-  public void removeClient(ClientServant cs) {
-    clientServants.remove(cs);
-  }
+            while (!this.serverSocket.isClosed()) {
+                Socket clientSocket = this.serverSocket.accept();
+
+                ClientServant cs = new ClientServant(clientSocket);
+                this.clientServants.add(cs);
+
+                cs.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        System.out.println("My Address: " + InetAddress.getLocalHost().getHostAddress() + ":" + port);
+        ChatServer.getApp().start(port);
+    }
+
+    public void removeClient(ClientServant cs) {
+        clientServants.remove(cs);
+    }
 }

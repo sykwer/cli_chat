29d42fb2479f4b8e079283712e7b293ff4f440a7
sykwer <sykwer@gmail.com>
2018-05-24 16:58:11 +0900

    reformat

--- src/server/ClientServant.java
+++ src/server/ClientServant.java
@@ -9,99 +9,99 @@ import java.io.PrintWriter;
 
 class ClientServant extends Thread {
 
-  private Socket clientSocket;
-  private Boolean isLoggedIn;
-  private String username;
-  private ChatServer server = ChatServer.getApp();
+    private Socket clientSocket;
+    private Boolean isLoggedIn;
+    private String username;
+    private ChatServer server = ChatServer.getApp();
 
-  public ClientServant(Socket socket) {
-    this.clientSocket = socket;
-  }
-
-  public void run() {
-    try {
-      InputStream is = this.clientSocket.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-      String line;
-      while (!this.clientSocket.isClosed() && (line = reader.readLine()) != null) {
-        String[] cmd = line.split(" ", 2);
-        handleCommand(cmd);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    public ClientServant(Socket socket) {
+        this.clientSocket = socket;
     }
-  }
 
-  private void handleCommand(String[] cmd) {
-    switch (cmd[0]) {
-      case "login":
-        login(cmd[1]);
-        break;
-      case "logout":
-        logout();
-        break;
-      case "send":
-        broadcastMsg(cmd[1]);
-        break;
-      case "list":
-        listClients();
-        break;
-      default:
+    public void run() {
+        try {
+            InputStream is = this.clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
+            String line;
+            while (!this.clientSocket.isClosed() && (line = reader.readLine()) != null) {
+                String[] cmd = line.split(" ", 2);
+                handleCommand(cmd);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void login(String username) {
-    sendStringToAllClients(String.format("%s has entered this chat room.", username));
-    isLoggedIn = true;
-    this.username = username;
-  }
+    private void handleCommand(String[] cmd) {
+        switch (cmd[0]) {
+            case "login":
+                login(cmd[1]);
+                break;
+            case "logout":
+                logout();
+                break;
+            case "send":
+                broadcastMsg(cmd[1]);
+                break;
+            case "list":
+                listClients();
+                break;
+            default:
 
-  private void logout() {
-    try {
-      sendStringToAllClients(String.format("%s left.", username));
-      server.removeClient(this);
-      clientSocket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
+        }
     }
-  }
 
-  private void broadcastMsg(String msg) {
-    sendStringToAllClients(String.format("%s: %s", username, msg));
-  }
-
-  private void listClients() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("List of clients:\n");
-    ArrayList<ClientServant> clients  = server.getClientServants();
-    for (ClientServant client: clients) {
-      sb.append(String.format("\t%s\n", client.getUsername()));
+    private void login(String username) {
+        sendStringToAllClients(String.format("%s has entered this chat room.", username));
+        isLoggedIn = true;
+        this.username = username;
     }
-    this.sendString(sb.toString());
-  }
 
-  private void sendStringToAllClients(String str) {
-    ArrayList<ClientServant> clients  = server.getClientServants();
-    for (ClientServant client: clients) {
-      client.sendString(str);
+    private void logout() {
+        try {
+            sendStringToAllClients(String.format("%s left.", username));
+            server.removeClient(this);
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void sendString(String str) {
-    try {
-      OutputStream os = clientSocket.getOutputStream();
-      PrintWriter writer = new PrintWriter(os);
-      writer.println(str);
-      writer.flush();
-    } catch (IOException e) {
-      e.printStackTrace();
+    private void broadcastMsg(String msg) {
+        sendStringToAllClients(String.format("%s: %s", username, msg));
     }
-  }
 
-  public String getUsername() {
-    return username;
-  }
+    private void listClients() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("List of clients:\n");
+        ArrayList<ClientServant> clients = server.getClientServants();
+        for (ClientServant client : clients) {
+            sb.append(String.format("\t%s\n", client.getUsername()));
+        }
+        this.sendString(sb.toString());
+    }
+
+    private void sendStringToAllClients(String str) {
+        ArrayList<ClientServant> clients = server.getClientServants();
+        for (ClientServant client : clients) {
+            client.sendString(str);
+        }
+    }
+
+    private void sendString(String str) {
+        try {
+            OutputStream os = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(os);
+            writer.println(str);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
 
 }
